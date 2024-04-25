94fb8f19e4c6ed494f5933f624ca271b9ea2392b
htmos6 <mehmet.arslan4000@gmail.com>
2024-04-25 23:14:58 +0300

    Gateway TcpClient property implemented and Tested

---
+++ LoRaWAN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoRaWAN
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Gateway gateway = new Gateway();
+
+            gateway.SendFramesToServer("127.0.0.1", 8080, "Hello, LoRaWAN Community!");
+        }
+    }
+}
